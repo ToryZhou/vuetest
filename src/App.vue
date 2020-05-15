ebe0755ff08be72c8a42f53985e675f211b9fc9e
toryzhou <522617505@qq.com>
2020-05-15 13:33:50 +0800

    Add vue test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="base">
+
+    <div id="nav" class="header">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="body">
+      <router-view/>
+    </div>
+    <div class="footer" ref="footer">zzzzzzzzzz</div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name: 'Hello',
+  mounted () {
+    console.log(this.$refs.footer)
   }
 }
+</script>
+
+<style lang="scss" src="./AppStyle.scss">
+
 </style>
